53ff7ef16a1b2fb8772e779d3e843ae6187597fc
Alex Fedotov <alxfed@gmail.com>
2019-09-03 07:36:31 -0500

    comments

--- hello_ymehcla.py
+++ hello_ymehcla.py
@@ -6,7 +6,7 @@ from sqlalchemy import *
 from datetime import datetime
 
 ABS_PATH = '/media/alxfed/toca/dbase/secondbase.sqlite' # the absolute path to a database
-ymehcla_path = f'sqlite:///{ABS_PATH}' # notice the _three_ slashes before the absolute path!
-
+ymehcla_path = f'sqlite:///{ABS_PATH}'  # notice the _three_ slashes before the absolute path!
+                                        # ':memory:' is an option too.
 
 metadata = MetaData(ymehcla_path)
